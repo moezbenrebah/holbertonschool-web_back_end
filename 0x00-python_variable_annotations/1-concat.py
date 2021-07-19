c647f599a4397bac65d6f357095c7f7d6663b4a6
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-19 04:41:10 +0100

    concatenate 2 strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is the "1-concat" module.
+The 1-concat module function: concat(str1: str, str2: str) -> str.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat: concatenate 2 strings
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+    Return:
+        string
+    """
+    return str1 + str2
