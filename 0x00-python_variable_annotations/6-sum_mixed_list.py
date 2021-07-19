ffaaba693d8535efb34a952e8ca66875e1dfe9e5
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-19 13:36:38 +0100

    fix pycodestyle error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,8 @@
 
 """
 This is the "6-sum_mixed_list" module.
-The 6-sum_mixed_list module function: sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float.
+The 6-sum_mixed_list module function:
+sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float.
 """
 
 from typing import List, Union
@@ -11,7 +12,7 @@ from typing import List, Union
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     sum_mixed_list: takes a list of floats and integers and return the sum.
-    
+
     Args:
         mxd_lst (List): a list of floats and integers
     Return:
