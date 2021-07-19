cb231730fdb0eedcac0f77e217a98897be47f7e5
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-19 05:10:58 +0100

    the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This is the "3-to_str" module.
+The 3-to_str module function: to_str(n: float) -> str.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: returns the string representation of the float.
+
+    Args:
+        n (float): a float
+    Return:
+        string
+    """
+    return str(n)
