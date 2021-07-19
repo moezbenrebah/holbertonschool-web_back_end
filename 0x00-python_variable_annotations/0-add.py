8f9277f646cb432d5a27a1617f5bceb55968f4a4
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-19 04:29:28 +0100

    add 2 floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is the "0-add" module.
+The 0-add module function: add(a: float, b: float) -> float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add: add 2 floats
+
+    Args:
+        a (float): first float
+        b (float): second float
+    Return:
+        float
+    """
+    return a + b
