35dc92c186bfc56df65c61b8190c8e75d8853362
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-19 04:57:22 +0100

    returns the floor of a flaot

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is the "2-floor" module.
+The 2-floor module function: floor(n: float) -> int.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor: calculate the floor of a given float
+
+    Args:
+        n (float): a float
+    Return:
+        integer
+    """
+    return math.floor(n)
