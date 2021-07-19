98aa944cacd32df0fb69256b4c4606f4497b0919
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-19 23:31:16 +0100

    fix doc strings

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""a type-annotated function safely_get_value"""
 
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """function zoom_array"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
