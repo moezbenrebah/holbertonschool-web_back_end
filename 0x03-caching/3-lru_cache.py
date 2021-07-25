76169545f86abdc8418d210476983b0f68bc4572
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-25 11:47:12 +0100

    implement lru caching

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""lifo_cache module"""
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache class"""
+    def __init__(self):
+        """Initializing"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+        self.cache_data = OrderedDict(self.cache_data)
+        self.cache_data.move_to_end(key)
+
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            removedItem = self.cache_data.popitem(last=False)
+            print("DISCARD:", removedItem)
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        if key or key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
