345a1d593418e95d389450f7be7c50d2ca61fcef
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-25 13:42:47 +0100

    update code

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -22,6 +22,7 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict(self.cache_data)
 
         if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            self.cache_data.move_to_end(key)
             removedItem = list(self.cache_data.popitem(last=False))[0]
             print("DISCARD:", removedItem)
 
