a6810c34f847939a839fcc204eeef7f874d1f294
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-07-28 18:12:35 +0100

    do not miss any item from a dataset

---
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        get_hyper_index: ensures that if between two queries, certain rows
+        are removed from the dataset, the user does not miss items from dataset
+        when changing page
+        """
+        assert isinstance(index, int) and index < len(self.indexed_dataset())
+        next_index = index + page_size
+        data = []
+        for element in range(index, next_index):
+            if not self.indexed_dataset().get(element):
+                element += 1
+                next_index += 1
+            data.append(self.indexed_dataset()[element])
+        return {'index': index, 'next_index': next_index,
+                'page_size': page_size, 'data': data}
