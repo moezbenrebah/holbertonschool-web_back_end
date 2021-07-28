f6836e9ca8c0e00f9edf9dab6dcc74b39953d960
Boul3ez85 <benrebah.moez@gmail.com>
2021-07-28 16:07:57 +0100

    fix values declaration

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -40,15 +40,18 @@ class Server:
         return self.__dataset[correct_index[0]:correct_index[1]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """"""
+        """
+        get_hyper: returns a dictionary containing nemours key value data
+        """
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
         next_page, prev_page = None, None
+
         if page < total_pages:
             next_page = page + 1
         if page > 1:
             prev_page = page - 1
 
-        return {'page_size': len(data), 'page': page, 'data': data,
+        return {'page_size': page_size, 'page': page, 'data': data,
                 'next_page': next_page, 'prev_page': prev_page,
                 'total_pages': total_pages}
