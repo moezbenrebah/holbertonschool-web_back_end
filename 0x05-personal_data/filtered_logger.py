a0eb7cf7b41680e9e065698b5ea8ec2cfac68743
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-01 08:14:33 +0100

    Update filtered_logger.py

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -15,7 +15,7 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = list(fields)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """filter values in incoming log records using filter_datum method"""
