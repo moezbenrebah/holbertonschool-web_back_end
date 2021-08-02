ca9a019fbb5d8f7d67df038d3c20cf10c3fa048b
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-02 10:33:16 +0100

    check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -11,3 +11,12 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password"""
+
+    hashed_password = hash_password(password)
+    if bcrypt.checkpw(password.encode('utf8'), hashed_password):
+        return True
+    return False
