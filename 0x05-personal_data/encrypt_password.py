85c1c643042391869c700bb8986327c59725e409
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-02 10:38:15 +0100

    fix code

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -16,8 +16,6 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """validate that the provided password matches the hashed password"""
 
-    hashed_password = hash_password(password)
     if bcrypt.checkpw(password.encode('utf8'), hashed_password):
         return True
-    else:
-        return False
+    return False
