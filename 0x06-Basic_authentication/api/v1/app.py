36c2772bf6e24d98218cb4bfd1561a77a991c620
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-05 00:40:30 +0100

    fix a typo

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -16,8 +16,8 @@ auth = None
 
 if os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
-    auth == Auth()
-        
+    auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
