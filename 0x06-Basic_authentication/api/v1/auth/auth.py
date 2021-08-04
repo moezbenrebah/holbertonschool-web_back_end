fdfa4d4d5d77e3c7de31863f7cdc6307aef17a85
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-04 20:53:39 +0100

    define routes

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -7,9 +7,22 @@ from typing import List, TypeVar
 
 class Auth:
     """Auth class"""
+
+    def __init__(self):
+        """Constructor"""
+        pass
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """return path, excluded_path, otherwise False"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """return request, otherwise None"""
