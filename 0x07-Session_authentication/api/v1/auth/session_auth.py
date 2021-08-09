cf8dcfe556366eca0b9d1968e453fbe51fa25b98
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-09 23:08:11 +0100

    return user from cookie value

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,9 @@ class SessionAuth(Auth):
         if session_id is None and not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        cookie_value = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie_value)
+        return User.get(user_id)
