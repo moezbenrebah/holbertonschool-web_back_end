15a5362db492a588724f9d7fc1b0fc888bff26ac
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-09 20:15:11 +0100

    user based on session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,6 @@
 """session authentication module"""
 
 from api.v1.auth.auth import Auth
-from datetime import datetime
 import uuid
 from models.user import User
 
@@ -21,3 +20,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None and not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
