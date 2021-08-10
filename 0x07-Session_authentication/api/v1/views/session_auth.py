7bb84ef392577e71e670f2980b4aeb06b84ba6e1
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-10 21:32:50 +0100

    create POST route

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""session route"""
+
+from models.user import User
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+import os
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def parse_request() -> str:
+    """create a route POST /auth_session/login"""
+    email = request.form.get('email')
+    if email == "" or email is None:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+    if password == "" or password is None:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})
+
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_value = auth.create_session(objs[0].id)
+    result = jsonify(objs[0].to_json())
+    session_key = os.getenv("SESSION_NAME")
+    result.set_cookie(session_key, session_value)
+    return result
