dce9692f98ea198b6686bfac69f6ed6381448370
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-10 12:42:06 +0100

    fix typo

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -50,4 +50,4 @@ class Auth:
         if request is None:
             return None
         _my_session_id = os.getenv('SESSION_NAME')
-        return request.cookies.get('_my_session_id')
+        return request.cookies.get(_my_session_id)
