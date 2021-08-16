eb5a371b9b706c5eaee835c412355f68742be200
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-16 23:40:17 +0100

    fix private module function syntax

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ def _hash_password(password: str) -> bytes:
     return hashed
 
 
-def _generate_uuid() -> str:
+def __generate_uuid() -> str:
     """return a string representation of a new UUID"""
     return uuid.uuid1()
 
