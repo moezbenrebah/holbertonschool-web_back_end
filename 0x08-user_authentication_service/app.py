516c39825bff23a365f1476cc4ee99287b621504
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-16 21:15:49 +0100

    create user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 """route module for API"""
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def hello():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """implements the POST /users route."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        check_user = AUTH.register_user(email, password)
+        if check_user:
+            return jsonify({"email": email, "message": "user created"})
+
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
