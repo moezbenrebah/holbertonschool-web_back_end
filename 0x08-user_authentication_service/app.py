50ceaf4de5aa892be4a208ebd0ff64f918c8ef58
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-17 12:22:46 +0100

    log in

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """route module for API"""
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, make_response, request
 
 
 app = Flask(__name__)
@@ -28,6 +28,24 @@ def users():
 
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """create a new session for the user, store it the session ID as a cookie
+    with key "session_id" on the response and return a JSON payload
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    valid_cred = AUTH.valid_login(email=email, password=password)
+    if valid_cred:
+        session_ID = AUTH.create_session(email)
+        response = make_response({"email": "email",
+                                  "message": "logged in"})
+        response.set_cookie("session_id", session_ID)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
