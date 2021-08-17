31590b84daa020cc6c040908d61afa9d7d7876c1
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-17 15:54:56 +0100

    user profile

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -63,5 +63,17 @@ def logout():
         abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """respond with 200 status code if the user exist,
+    otherwise return 403 HTTP status"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
