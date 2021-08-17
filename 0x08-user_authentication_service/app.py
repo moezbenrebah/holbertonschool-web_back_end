472d0c93f1de44ba61bc44dbdf8ea7554136a0f6
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-17 17:50:51 +0100

    fix code

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -98,8 +98,7 @@ def update_password():
     new_password = request.form.get("new_password")
 
     try:
-        AUTH.update_password(reset_token=reset_token,
-                             password=new_password)
+        AUTH.update_password(reset_token,new_password)
         return jsonify({"email": email,
                         "message": "Password updated"}), 200
 
