1acdf94a24cb818c487b85233c25e496c6b2a895
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-24 11:04:21 +0100

    mock http call

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """test_utils test module"""
 
+import json
 import unittest
+from unittest.mock import patch
+import requests
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,6 +28,21 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         """test that a KeyError is raised for a given inputs"""
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson class"""
+
+    @parameterized.expand([
+        ({"test_url": "http://example.com"}, {"test_payload": True}),
+        ({"test_url": "http://holberton.io"}, {"test_payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """"""
+        with patch('requests.get') as mock_request:
+            mock_request.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_request.assert_called_once()
 
 
 if __name__ == '__main__':
