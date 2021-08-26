e62888d68a89a0e3d338c77606e41e8df0f903c5
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-26 16:33:20 +0100

    update code

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -3,20 +3,20 @@
 
 from flask import Flask, request, render_template
 from flask_babel import Babel, _
-Config = __import__('1-app').Config
+#Config = __import__('1-app').Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """match the best language to support"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
+#@babel.localeselector
+#def get_locale():
+#    """match the best language to support"""
+#    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-app.config.from_object('3-app.Config')
+#app.config.from_object('3-app.Config')
 
 
 @app.route('/')
