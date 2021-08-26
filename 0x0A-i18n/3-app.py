91e4b1532dd197366dcd6eb8bc80bdeac0465ed5
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-26 18:30:09 +0100

    update code

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -2,7 +2,7 @@
 """route module"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 babel = Babel(app)
