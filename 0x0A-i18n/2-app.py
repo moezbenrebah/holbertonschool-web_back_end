19094058756dcedec3fc00da06ba79d8048f5599
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-26 11:35:22 +0100

    fix module doc string

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+"""route module"""
 
 from flask import Flask, request, render_template
 from flask_babel import Babel
@@ -18,3 +18,7 @@ def get_locale():
 def index():
     """display 2-index.html page"""
     return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
