a25ead73041e3d069139b26ca09d292a27601463
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-26 18:11:07 +0100

    fix code

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 """route module"""
 
-from flask import Flask, request, render_template
-from flask_babel import Babel, gettext
-Config = __import__('1-app').Config
-
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """match the best language to support"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
+class Config(object):
+    """ Config class for Babel object """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object('3-app.Config')
+app.config.from_object(Config)
 
 
 @app.route('/')
-def index():
-    """display 3-index.html"""
+def hello():
+    """ render a basic html file """
     return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ a function to determine the best match with the supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
