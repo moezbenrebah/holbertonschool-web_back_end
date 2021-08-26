acd9174971ade7cb98a3c4ed804a539a518560ac
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-26 16:37:21 +0100

    fix code

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -3,17 +3,24 @@
 
 from flask import Flask, request, render_template
 from flask_babel import Babel, _
-Config = __import__('1-app').Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-#@babel.localeselector
-#def get_locale():
-#    """match the best language to support"""
-#    return request.accept_languages.best_match(Config.LANGUAGES)
+class Config:
+    """configure available languages in the app"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """match the best language to support"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 app.config.from_object('3-app.Config')
