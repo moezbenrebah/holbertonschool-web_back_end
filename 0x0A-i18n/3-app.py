a043effe5e6aa4e377abeb98ce52e6e320d65b6f
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-26 16:07:00 +0100

    parametrize templates

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""route module"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel, _
+Config = __import__('1-app').Config
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """match the best language to support"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+app.config.from_object('3-app.Config')
+
+
+@app.route('/')
+def index():
+    """display 3-index.html"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
