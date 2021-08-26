817678c41ebcd7457dc342f2413f30a2d76d03e2
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-26 16:40:08 +0100

    update code

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -3,18 +3,11 @@
 
 from flask import Flask, request, render_template
 from flask_babel import Babel, _
+Config = __import__('1-app').Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
-
-
-class Config:
-    """configure available languages in the app"""
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 @babel.localeselector
@@ -30,7 +23,3 @@ app.config.from_object('3-app.Config')
 def index():
     """display 3-index.html"""
     return render_template('3-index.html')
-
-
-if __name__ == '__main__':
-    app.run()
