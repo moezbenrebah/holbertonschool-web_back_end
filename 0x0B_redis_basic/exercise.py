88e40beca323bd8dbeab1b0cc8997cb8560aeef4
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-08-31 12:25:23 +0100

    change uuid version

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,7 +18,7 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a random key and store value of that key"""
 
-        random_key = str(uuid.uuid1())
+        random_key = str(uuid.uuid4())
         s = self._redis.set(random_key, data)
         return random_key
 
