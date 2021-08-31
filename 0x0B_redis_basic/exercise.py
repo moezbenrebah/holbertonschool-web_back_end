0eda9246e14f9b1e6db9430aef2aa9dc35f915b5
Boul3ez85 <benrebah.moez@gmail.com>
2021-08-31 12:42:34 +0100

    recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """redis-py module"""
 
-from typing import Callable, Union
+from typing import Callable, Optional, Union
 import redis
 import uuid
 
@@ -22,20 +22,17 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str,
-            **fn: Callable) -> Union[str, bytes, int, float]:
-        """return the value of key"""
-
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """return back the data value in desirable format"""
         if fn:
             return fn(self._redis.get(key))
-        return self._redis.get(key)
+        else:
+            return self._redis.get(key)
 
     def get_str(self, key):
         """return key value as a string"""
-
         return get(key, str)
 
     def get_int(self, key):
         """return key value as an integer"""
-
         return get(key, int)
