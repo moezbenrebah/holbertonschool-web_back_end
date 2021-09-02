138095c46183c3029f1912bb78002258cf16fa52
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-02 11:13:27 +0100

    old school band

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- list all bands with 'Glam rock' style
+-- ranked by their longevity
+
+
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
