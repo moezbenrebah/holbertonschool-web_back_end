e8312ed5a673388453e183f369eee77d9c970aa2
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-07 04:52:26 +0100

    log stats

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Nginx logs in MongoDB module"""
+
+from pymongo import MongoClient
+
+
+def log_infos():
+    """print nginx logs in mongodb"""
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    client = MongoClient('mongodb://localhost:27017')
+    nglogs = client.logs.nginx
+
+    print(f'{nglogs.count_documents({})} logs')
+
+    print("Methods:")
+
+    for method in methods:
+        print(f'\tmethod {method}:
+              {nglogs.count_documents({"method": method})}')
+
+    print(f'{nglogs.count_documents({"method": "GET",
+                                     "path": "/status"})} status check')
+
+
+if __name__ == '__main__':
+    log_infos()
