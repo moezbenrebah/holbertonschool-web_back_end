8536292d97ba1a411165f675bba63c1cd12f29b7
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-07 02:44:23 +0100

    change attribute value

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/use/bin/env python3
+"""find and update a document module"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """find an attribute and change its value"""
+
+    if mongo_collection:
+        mongo_collection.update_many({"name": name}, { '$set': { "topics": topics} })
