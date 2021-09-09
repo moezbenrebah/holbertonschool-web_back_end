4fec77143a36bf14ea62c3c14a9a7e675382d656
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-09 23:28:32 +0100

    update code

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,8 +1,10 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-  for (const [k, v] of Object.entries(employeesList)) {
-    allEmployees[`${k}`] = v;
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
-
-  return { allEmployees, getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length };
-};
+}
