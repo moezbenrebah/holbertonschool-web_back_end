ac85b47b0c6d23704d810ed5d12de509aaa8cae1
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-09 01:59:09 +0100

    fix indentation

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,10 +1,9 @@
 export default function createReportObject(employeesList) {
-	const allEmployees = {}
-	const entries = Object.entries(employeesList)
-	for (const [k, v] of entries) {
-		allEmployees[`${k}`] = v;
-	}
-	return {allEmployees,
-		getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length
-	};
+  const allEmployees = {}
+  const entries = Object.entries(employeesList)
+  for (const [k, v] of entries) {
+    allEmployees[`${k}`] = v;
+  };
+
+  return { allEmployees, getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length };
 }
