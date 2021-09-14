bd05fb49b939296a8c6f99b025b231db283613a0
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-14 22:17:03 +0100

    fix syntax

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    reject(Error(`${ filename } cannot be processed`));
+    reject(Error(`${filename} cannot be processed`));
   });
 }
 
