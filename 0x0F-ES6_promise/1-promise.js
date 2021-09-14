4ed801b2ad07ac9a1350fc4c114f057e996dca3a
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-14 20:32:34 +0100

    update code

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (!success) {
-      reject(Error('The fake API is not working currently'));
-    } else {
+    if (success) {
       resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
