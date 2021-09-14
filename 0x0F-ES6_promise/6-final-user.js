6eb7bdad91c3edbfa24360805a05801a3f9ebf7f
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-14 23:10:10 +0100

    fix code

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,15 +2,9 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = await signUpUser(firstName, lastName).then((data) => ({
-    status: 'fulfilled',
-    value: data,
-  }));
+  const signUp = await signUpUser(firstName, lastName).then((data) => ({ status: 'fulfilled', value: data }));
 
-  const photo = await uploadPhoto(fileName).catch((error) => ({
-    status: 'rejected',
-    value: error.toString(),
-  }));
+  const photo = await uploadPhoto(fileName).catch((error) => ({ status: 'rejected', value: error.toString() }));
 
   return [signUp, photo];
 }
