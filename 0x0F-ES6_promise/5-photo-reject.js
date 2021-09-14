a4cb0b22526d07e31d3adcf054a438b33ea4e1a1
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-14 22:36:07 +0100

    fix code

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    reject(Error(`${filename} cannot be processed`));
+    reject(new Error(`${filename} cannot be processed`));
   });
 }
 
