39e8ab2de4640b6f089c35136f5e9a02057da487
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-16 00:59:52 +0100

    fix syntax

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,5 @@
 export default class HolbertonCourse {
-  constructor (name, length, students) {
+  constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
     }
