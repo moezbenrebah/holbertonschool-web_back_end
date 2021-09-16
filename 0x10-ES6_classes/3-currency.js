b440d8dc3b0d8582b2c7ebdc7ba27e09b7840d38
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-16 01:08:14 +0100

    fix syntax

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,5 +1,12 @@
 export default class Currency {
   constructor(code, name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
     this._code = code;
     this._name = name;
   }
