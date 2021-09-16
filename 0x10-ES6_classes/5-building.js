a4187ce35e7d93bcf50f1780b832985e5106c1da
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-16 02:02:43 +0100

    update code

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,11 +1,10 @@
 export default class Building {
-  contructor(sqft) {
+  constructor(sqft) {
     if (this.constructor !== Building && !(this.evacuationWarningMessage)) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
-
     if (typeof sqft !== 'number') {
-      throw TypeError('sqft must be a number');
+      throw (TypeError('sqft must be an integer'));
     }
     this._sqft = sqft;
   }
