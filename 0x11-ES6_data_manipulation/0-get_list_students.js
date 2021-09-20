4a55ae52a222c52247dc4edbaab3c63df160ce5c
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-21 00:31:40 +0100

    update code

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,23 @@
 export default function getListStudents() {
-  let students = new Array();
+  const students = [];
 
-  let student1 = {};
+  const student1 = {};
   student1.id = 1;
   student1.firstName = 'Guillaume';
   student1.location = 'San Francisco';
   students.push(student1);
 
-  let student2 = {};
+  const student2 = {};
   student2.id = 2;
   student2.firstName = 'James';
   student2.location = 'Columbia';
   students.push(student2);
 
-  let student3 = {};
+  const student3 = {};
   student3.id = 5;
   student3.firstName = 'Serena';
   student3.location = 'San Francisco';
   students.push(student3);
 
   return students;
-} 
+}
