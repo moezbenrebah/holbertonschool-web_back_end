6737cbab7eb8d3bdc123e446b37e96b22a01527b
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-21 00:44:34 +0100

    update code

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(arrObj) {
-  if (arrObj.length !== 0) {
-    return arrObj.reduce((sum, { id }) => sum + id, 0);
-  }
+  return arrObj.reduce((sum, { id }) => sum + id, 0);
 }
