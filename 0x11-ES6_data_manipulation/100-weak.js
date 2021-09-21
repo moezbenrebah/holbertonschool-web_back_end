ae5a1429a4f1659930751e14977dd6d692a6b0d6
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-21 22:46:16 +0100

    update code

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -3,10 +3,10 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   let result = weakMap.get(endpoint);
 
-  if (result) {
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  } else {
+  if (!(result)) {
     weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   }
 
   if (result >= 5) {
