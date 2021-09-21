6c36fa6ab4e336e668ccf9913fd2c9b86eace108
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-21 19:51:32 +0100

    update code

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string') return '';
-  if (startString.length === 0) return '';
-  if (typeof set !== 'object') return '';
+  if (typeof startString !== 'string' || startString.length === 0 || typeof set !== 'object') return '';
 
   const arr = [];
 
   set.forEach((item) => {
-    if (item.startsWith(startString)) {
-      arr.push(item.substr(startString.length));
+    if (item && item.startsWith(startString)) {
+      arr.push(item.substring(startString.length));
     }
   });
 
