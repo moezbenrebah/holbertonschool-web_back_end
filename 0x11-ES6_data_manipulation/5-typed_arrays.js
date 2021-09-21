d8e184a9ee39f3f1c36590bdd574bf2477f0ebf4
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-21 12:47:25 +0100

    update code

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
-  if (view.length === 0) {
+  try {
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer);
+    view.setInt8(position, value);
+    return view;
+  } catch (error) {
     throw new Error('Position outside range');
   }
-  view.setInt8(position, value);
-  return view;
 }
