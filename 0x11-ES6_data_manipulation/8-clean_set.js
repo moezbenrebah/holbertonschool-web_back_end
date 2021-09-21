a1c93cc83ce9ad16e419dcdfe0d3b7123f287b6f
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-21 19:44:02 +0100

    update conditions

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string') return '';
   if (startString.length === 0) return '';
+  if (typeof set !== 'object') return '';
 
   const arr = [];
 
