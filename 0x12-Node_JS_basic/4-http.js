02765dd05f086ca7eef5350bdf69eaf47d67aa5f
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-26 00:56:23 +0100

    simple server

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const host = "127.0.0.1";
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => console.log(`Server is running...`))
+
+
+module.exports = app;
