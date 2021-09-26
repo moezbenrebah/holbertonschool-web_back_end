4cc03ffb8e6a3b989b060cf0c8c80e07a0db89a1
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-26 22:28:20 +0100

    fix syntax

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -22,6 +22,7 @@ const app = http.createServer(async (req, res) => {
 });
 
 
-app.listen(port, host, () => { console.log('Server is running...') });
-
+app.listen(port, host, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
 module.exports = app;
