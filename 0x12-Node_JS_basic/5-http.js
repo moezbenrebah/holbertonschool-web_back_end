42ca43fda8592e182ba1a06477037cf26ee11bf1
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-26 22:24:33 +0100

    fix code

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,18 +1,27 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
+
+const host = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   }
   else if (req.url === '/students') {
-    res.end(`This is the list of our students ${countStudents(process.argv[2])}`)
+    let initParse = 'This is the list of our students\n';
+    await countStudents(process.argv[2]).then((result) => {
+      initParse += result;
+      res.end(initParse)
+    }).catch((error) => {
+      initParse += error.message;
+      res.end(initParse);
+    });
   }
 });
 
 
-app.listen(port, () => console.log('Server is running...'));
+app.listen(port, host, () => console.log('Server is running...'));
 
 module.exports = app;
