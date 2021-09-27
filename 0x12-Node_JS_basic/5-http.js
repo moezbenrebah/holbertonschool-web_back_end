3fd39bca5bec0e8ff9fb83ab2c3c2cd8bca316f0
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 16:28:45 +0100

    update code

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -2,6 +2,8 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.set('content-type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   }
