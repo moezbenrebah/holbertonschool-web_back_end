a4684261c97e763a007bb2bcc255669d634339a5
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 14:42:53 +0100

    Update 2-read_file.js

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
-const countStudents = (aPath) => {
+const countStudents = (filePath) => {
   try {
-    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
-    data = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${data.length}`);
-    const subjects = {};
-    for (const row of data) {
+    let data = fs.readFileSync(filePath, 'utf8').toString().split('\n');
+    const studentsArray = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${studentsArray.length}`);
+    const rows = {};
+    for (const row of studentsArray) {
       const student = row.split(',');
-      if (!subjects[student[3]]) subjects[student[3]] = [];
+      if (!rows[student[3]]) rows[student[3]] = [];
       subjects[student[3]].push(student[0]);
     }
-    for (const subject in subjects) {
-      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+    for (const cls in rows) {
+      if (cls) console.log(`Number of students in ${cls}: ${subjects[cls].length}. List: ${subjects[cls].join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
