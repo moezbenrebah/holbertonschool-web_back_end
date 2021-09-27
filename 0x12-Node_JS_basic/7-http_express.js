5bc2f32c29945660753550a75694bdce6d6efb3a
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 17:13:48 +0100

    update code

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -3,18 +3,16 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 
 app.get('/students', async (req, res) => {
-  let initParse = 'This is the list of our students\n';
-  await countStudents(process.argv[2]).then((result) => {
-    initParse += result;
-    res.end(initParse)
-  }).catch((error) => {
-    res.end(`This is the list of our students\n${error.message}`);
-  });
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
 
 app.listen(1245, () => console.log('Server is running...'));
