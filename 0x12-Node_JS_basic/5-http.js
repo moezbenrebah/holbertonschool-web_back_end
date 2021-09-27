4b674088d98755ce4a5aa84b85745f6cf121118f
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 16:13:43 +0100

    update code

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,7 +4,9 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  }
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
@@ -16,5 +18,6 @@ const app = http.createServer(async (req, res) => {
   }
   res.end();
 });
+
 app.listen(1245);
 module.exports = app;
