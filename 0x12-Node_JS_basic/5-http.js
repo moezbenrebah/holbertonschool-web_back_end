223ff20dfa09ea3173d317da827606a083c801f4
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 16:26:09 +0100

    update code

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,22 +1,24 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   }
   if (req.url === '/students') {
-    countStudents(process.argv[2])
+    let result = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
       .then((data) => {
-        const dt = `This is the list of our students\n${data}`;
-        res.end(dt);
+        result += data;
+        res.end(result);
       })
       .catch((error) => {
-        res.end(`This is the list of our students\n${error.message}`);
+        result += error.message;
+        res.end(result);
       });
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => console.log('Server is running...');
 
 module.exports = app;
