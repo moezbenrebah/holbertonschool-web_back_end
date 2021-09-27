d9c5e9ec0a5c8f58aecb83ff23e35e354f624420
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 16:02:51 +0100

    fix code

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -4,6 +4,7 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
