40e230fac91c812e523485f486c742b44afbf4a0
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 14:38:37 +0100

    update code

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,27 +1,22 @@
+
 const fs = require('fs');
 
-function countStudents(filePath) {
+const countStudents = (aPath) => {
   try {
-    const data = fs.readFileSync(filePath, { encoding: 'utf8' }).toString().split('\n');
-    const studentsArray = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${studentsArray.length}`);
-    const rows = {};
-    for (const row of studentsArray) {
-      let newRow = row.split(',');
-      if (!(rows[newRow[3]])) {
-        rows[newRow[3]] = [];
-      }
-      rows[newRow[3]].push(newRow[0]);
+    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
     }
-    
-    for (const cls in rows) {
-      if (cls) {
-        console.log(`Number of students in ${cls}: ${rows[cls].length}. List: ${rows[cls].join(', ')}`);
-      }
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
     }
-      } catch (err) {
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 };
-
 module.exports = countStudents;
