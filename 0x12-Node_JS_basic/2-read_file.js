b4fb63e35da8f66f8ecd698524c1079328ec6827
Boul3ez85 <benrebah.moez@gmail.com>
2021-09-27 14:36:49 +0100

    fix code

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -2,47 +2,26 @@ const fs = require('fs');
 
 function countStudents(filePath) {
   try {
-    const data = fs.readFileSync(filePath, { encoding: 'utf8' });
-    const studentList = [];
-    const arr = data.split(/\r?\n/);
-    const fieldName = arr[0].split(',');
-    const bodyContent = arr.slice(1);
-
-    for (let i = 0; i < bodyContent.length; i += 1) {
-      const result = bodyContent[i].split(',');
-      if (result.length === fieldName.length) {
-        const line = {};
-        for (let j = 0; j < fieldName.length; j += 1) {
-          line[fieldName[j].trim()] = result[j].trim();
-        }
-        studentList.push(line);
+    const data = fs.readFileSync(filePath, { encoding: 'utf8' }).toString().split('\n');
+    const studentsArray = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${studentsArray.length}`);
+    const rows = {};
+    for (const row of studentsArray) {
+      let newRow = row.split(',');
+      if (!(rows[newRow[3]])) {
+        rows[newRow[3]] = [];
+      }
+      rows[newRow[3]].push(newRow[0]);
+    }
+    
+    for (const cls in rows) {
+      if (cls) {
+        console.log(`Number of students in ${cls}: ${rows[cls].length}. List: ${rows[cls].join(', ')}`);
       }
     }
-
-    let csClass = 0;
-    let sweClass = 0;
-    const cs = [];
-    const swe = [];
-
-    studentList.forEach((n) => {
-      if (n.field === 'CS') {
-        csClass += 1;
-        cs.push(n.firstname);
-      } else if (n.field === 'SWE') {
-        sweClass += 1;
-        swe.push(n.firstname);
-      }
-    });
-
-    const countStudents = csClass + sweClass;
-
-    console.log(`Number of students: ${countStudents}`);
-    console.log(`Number of students in CS: ${csClass}. List: ${cs.toString().split(',').join(', ')}`);
-    console.log(`Number of students in SWE: ${sweClass}. List: ${swe.toString().split(',').join(', ')}`);
-
-  } catch (err) {
+      } catch (err) {
     throw new Error('Cannot load the database');
   }
-}
+};
 
 module.exports = countStudents;
