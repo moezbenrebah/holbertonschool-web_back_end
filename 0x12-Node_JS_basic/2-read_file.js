96cd34e0ee0e595d1da8f3c6169c9871ff9fb526
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-27 14:50:11 +0100

    update code

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const countStudents = (path) => {
   try {
     let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-    data = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${data.length}`);
+    let bodyContent = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${bodyContent.length}`);
     const rows = {};
-    for (const row of data) {
-      const student = row.split(',');
-      if (!rows[student[3]]) rows[student[3]] = [];
-      subjects[student[3]].push(student[0]);
+    for (const row of bodyContent) {
+      const line = row.split(',');
+      if (!rows[line[3]]) rows[line[3]] = [];
+      rows[line[3]].push(line[0]);
     }
     for (const cls in rows) {
-      if (cls) console.log(`Number of students in ${cls}: ${subjects[cls].length}. List: ${subjects[cls].join(', ')}`);
+      if (cls) console.log(`Number of students in ${cls}: ${rows[cls].length}. List: ${rows[cls].join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
