d7fa8718e56f6014126d50f5efdd5c0e0f085740
Moez <57503360+Boul3ez85@users.noreply.github.com>
2021-09-29 13:57:54 +0100

    update code

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,16 +1,19 @@
 const chai = require('chai');
-const sinon = require('sinon');
 const expect = chai.expect;
+const sinon = require('sinon');
 const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
-describe('sendPaymentRequestToApi', function() {
-  const spy = sinon.spy(Utils, 'calculateNumber');
+describe('sendPaymentRequestToApi function', function() {
+  const spy = sinon.spy(console, 'log');
 
   it('validate the usage of the Utils function', function() {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.withArgs('SUM', 100, 20).returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
+    expect(spyConsole.calledWith('The total is: 10')).to.be.true;
+    stub.restore()
     spy.restore();
   });
 });
