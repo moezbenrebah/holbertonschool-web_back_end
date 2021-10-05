3c8189cd4b00fdb3693f4daea227e8abe635c2f1
Boul3ez85 <benrebah.moez@gmail.com>
2021-10-05 22:43:46 +0100

    async operations

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,28 @@
+import { createClient } from 'redis';
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client is connected to the server')
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.toString}`)
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, (error, reply) => {
+        redis.print(`Reply: ${reply}`);
+    });
+}
+
+const displaySchoolValue = async (schoolName) => {
+    const reply = await promisify(client.get).bind(client);
+    console.log(reply);
+}
+
+
+(await () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}) ();
