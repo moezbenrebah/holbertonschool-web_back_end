d0e6223a8c5acfc1e19725f0dea15e865e77d61e
moezbenrebah <boul3ez07.23@gmail.com>
2025-08-07 08:25:24 +0100

    update code to accept 3 handlers

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  console.log('Got a response from the API');
-  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error());
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
